557aed5409f366a17cafec3d89224fa504202559
Marcin Drozd <marcindrozd@me.com>
2015-02-23 11:05:33 +0100

    Correct test for video model

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,9 +7,9 @@ describe Video do
                       small_img_url: "/futurama_sm.jpg",
                       large_img_url: "/futurama_lg.jpg")
     video.save
-    expect(video.title).to eq("Futurama")
-    expect(video.description).to eq("This is an awesome show")
-    expect(video.small_img_url).to eq("/futurama_sm.jpg")
-    expect(video.large_img_url).to eq("/futurama_lg.jpg")
+    expect(Video.first.title).to eq("Futurama")
+    expect(Video.first.description).to eq("This is an awesome show")
+    expect(Video.first.small_img_url).to eq("/futurama_sm.jpg")
+    expect(Video.first.large_img_url).to eq("/futurama_lg.jpg")
   end
 end
