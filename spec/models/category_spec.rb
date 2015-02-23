de5548ec598c9d53b76201c9852aab0d0a2e5446
Marcin Drozd <marcindrozd@me.com>
2015-02-23 11:50:39 +0100

    Clean up testing

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,28 +2,5 @@ require 'spec_helper'
 require 'shoulda/matchers'
 
 describe Category do
-  it "saves itself" do
-    category = Category.new(name: "Adventure")
-    category.save
-
-    expect(Category.first).to eq(category)
-  end
-
-  it "has many videos" do
-    category = Category.new(name: "Adventure")
-    indiana = Video.create(title: "Indiana Jones",
-                      description: "This is a great movie",
-                      small_img_url: "/indiana_sm.jpg",
-                      large_img_url: "/indiana_lg.jpg")
-    futurama = Video.create(title: "Futurama",
-                      description: "This is an awesome show",
-                      small_img_url: "/futurama_sm.jpg",
-                      large_img_url: "/futurama_lg.jpg")
-    category.videos << indiana
-    category.videos << futurama
-    category.save
-    expect(Category.first.videos).to eq([futurama, indiana])
-  end
-
   it { should have_many(:videos) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,38 +2,7 @@ require 'spec_helper'
 require 'shoulda/matchers'
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: "Futurama",
-                      description: "This is an awesome show",
-                      small_img_url: "/futurama_sm.jpg",
-                      large_img_url: "/futurama_lg.jpg")
-    video.save
-    expect(Video.first).to eq(video)
-  end
-
-  it "belongs to a category" do
-    video = Video.new(title: "Futurama",
-                      description: "This is an awesome show",
-                      small_img_url: "/futurama_sm.jpg",
-                      large_img_url: "/futurama_lg.jpg")
-    category = Category.new(name: "Comedy")
-    video.category = category
-    video.save
-
-    expect(Video.first.category).to eq(category)
-  end
-
-  it "requires a title" do
-    video = Video.create(title: "", description: "Fun movie!")
-    expect(Video.first).to eq(nil)
-    expect(Video.count).to eq(0)
-  end
-
-  it "requires a description" do
-    video = Video.create(title: "Futurama", description: "")
-    expect(Video.first).to eq(nil)
-    expect(Video.count).to eq(0)
-  end
-
   it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 end
