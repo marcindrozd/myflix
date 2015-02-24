2d67e12fe3ed4ea54d22999fddb2b728918bd6d9
Marcin Drozd <marcindrozd@me.com>
2015-02-24 11:25:25 +0100

    Additional test step for recent_videos method

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -27,5 +27,20 @@ describe Category do
 
       expect(category.recent_videos).to eq([titanic, family_guy, avengers, ghostbusters, south_park, indiana_jones])
     end
+
+    it "returns an empty array when category has no videos" do
+      category = Category.create(name: "Blockbusters")
+      other_category = Category.create(name: "Comedy")
+
+      indiana_jones = Video.create(title: "Indiana Jones", description: "A great adventure", category: category, created_at: 3.days.ago)
+      futurama = Video.create(title: "Futurama", description: "This is a space show", category: category, created_at: 4.days.ago)
+      avengers = Video.create(title: "Avengers", description: "Comic book battle", category: category)
+      south_park = Video.create(title: "South Park", description: "Quiet mountain town", category: category, created_at: 2.days.ago)
+      family_guy = Video.create(title: "Family Guy", description: "Great comedy", category: category)
+      ghostbusters = Video.create(title: "Ghostbusters", description: "Who ya gonna call", category: category, created_at: 1.day.ago)
+      titanic = Video.create(title: "Titanic", description: "Love story", category: category)
+
+      expect(other_category.recent_videos).to eq([])
+    end
   end
 end
