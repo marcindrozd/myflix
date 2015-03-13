30fe17c0c80134596520a3490779d990321db70a
Marcin Drozd <marcindrozd@me.com>
2015-03-13 15:48:32 +0100

    refactor code for updating params

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -7,7 +7,7 @@ class PasswordsController < ApplicationController
     @token = SecureRandom.urlsafe_base64
 
     if @user
-      @user.update_attributes(token: @token)
+      @user.update(token: @token)
       UserMailer.reset_password(@user, @token).deliver
       redirect_to confirm_password_reset_path
     else
@@ -31,9 +31,8 @@ class PasswordsController < ApplicationController
         flash.now[:danger] = "Password can't be blank!"
         render :reset_password
       else
-        @user.password = params[:password]
+        @user.update(token: nil, password: params[:password])
         flash[:success] = "Password reset was successful. You may now log in with your new password!"
-        @user.update_attributes(token: nil)
         redirect_to sign_in_path
       end
     else
