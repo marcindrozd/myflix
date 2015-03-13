a85184640b272a6066091a0f245d2fad8b62298b
Marcin Drozd <marcindrozd@me.com>
2015-03-13 16:22:58 +0100

    add configuration for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,4 +17,15 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for mailgun
+  ActionMailer::Base.smtp_settings = {
+    :port           => 587,
+    :address        => "smtp.mailgun.org",
+    :domain         => ENV['MAILGUN_DOMAIN'],
+    :user_name      => ENV['MAILGUN_USERNAME'],
+    :password       => ENV['MAILGUN_PASSWORD'],
+    :authentication => :plain,
+  }}
 end
