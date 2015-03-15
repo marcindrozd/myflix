b4856336cf1bfc536b5a18e3e67e93e0d10ec572
Marcin Drozd <marcindrozd@me.com>
2015-03-15 18:59:59 +0100

    refactor password reset feature test

--- spec/features/password_reset_spec.rb
+++ spec/features/password_reset_spec.rb
@@ -2,16 +2,17 @@ require 'spec_helper'
 
 feature "Using password reset feature" do
   scenario "user resets forgotten password" do
-    bob = Fabricate(:user, email_address: "bob@example.com")
+    bob = Fabricate(:user, password: "old_password", email_address: "bob@example.com")
     clear_emails
 
     visit root_path
 
     send_password_reset_email_to("bob@example.com")
     open_email_and_click_password_reset(bob, "bob@example.com")
-    fill_in_new_password("new_password")
+    fill_in_new_password("new password")
+    sign_in_with_new_password("bob@example.com", "new password")
 
-    sign_in(bob)
+    expect(page).to have_content("Welcome, #{bob.full_name}")
   end
 end
 
@@ -31,4 +32,11 @@ end
 def fill_in_new_password(password)
   fill_in('New Password', with: password)
   click_button('Reset Password')
+end
+
+def sign_in_with_new_password(email, password)
+  visit('/sign_in')
+  fill_in('Email address', :with => "bob@example.com")
+  fill_in('Password', :with => "new password")
+  click_button('Sign in')
 end
