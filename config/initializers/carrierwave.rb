677113a8607ad90720df409a03dd2b6bb6738fdb
Marcin Drozd <marcindrozd@me.com>
2015-03-22 11:41:08 +0100

    split s3 into development and production parts

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,25 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['S3_MYFLIX_ACCESS_KEY'],
-    aws_secret_access_key: ENV['S3_MYFLIX_SECRET_ACCESS_KEY'],
-    region: ENV['S3_MYFLIX_REGION'],
-  }
-
-  if Rails.env.test?
+  if Rails.env.development?
+    config.storage = :fog
+    config.fog_directory = ENV['S3_MYFLIX_STG_DIRECTORY']
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_MYFLIX_STG_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_MYFLIX_STG_SECRET_ACCESS_KEY'],
+      region: ENV['S3_MYFLIX_STG_REGION'],
+    }
+  elsif Rails.env.production?
+    config.storage = :fog
+    config.fog_directory = ENV['S3_MYFLIX_DIRECTORY']
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_MYFLIX_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_MYFLIX_SECRET_ACCESS_KEY'],
+      region: ENV['S3_MYFLIX_REGION'],
+    }
+  else
     config.storage = :file
     config.enable_processing = false
     config.root = "#{Rails.root}/tmp"
-  else
-    config.storage = :fog
   end
-
-  config.fog_directory = ENV['S3_MYFLIX_DIRECTORY']
 end
