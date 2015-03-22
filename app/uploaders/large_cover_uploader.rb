306b55a8c9a5de0eff7441e07fc94080515ad72a
Marcin Drozd <marcindrozd@me.com>
2015-03-22 11:50:59 +0100

    create random filenames for uploaded pictures

--- app/uploaders/large_cover_uploader.rb
+++ app/uploaders/large_cover_uploader.rb
@@ -3,7 +3,14 @@ class LargeCoverUploader < CarrierWave::Uploader::Base
 
   process resize_to_fill: [665, 375]
 
-  def default_url(*args)
-    "https://md-myflix.s3.amazonaws.com/uploads/default_large.png"
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 end
--- app/uploaders/small_cover_uploader.rb
+++ app/uploaders/small_cover_uploader.rb
@@ -3,7 +3,14 @@ class SmallCoverUploader < CarrierWave::Uploader::Base
 
   process resize_to_fill: [166, 236]
 
-  def default_url(*args)
-    "https://md-myflix.s3.amazonaws.com/uploads/default_small.png"
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 end
