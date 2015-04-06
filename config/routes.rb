fd10da291005b9cf005d1a2686323d33900c4845
Marcin Drozd <marcindrozd@me.com>
2015-04-06 16:27:06 +0200

    add admin view payments page

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Myflix::Application.routes.draw do
 
   namespace :admin do
     resources :videos, only: [:new, :create]
+    resources :payments, only: [:index]
   end
 
   get '/forgot_password', to: "passwords#forgot_password"
